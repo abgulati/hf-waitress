aa70c21fac04b616338d74d2c8ab7b0eb71b25e0
AbheekG <abheekg@hotmail.com>
2024-07-26 18:17:47 -0700

    first commit - foundations

---
+++ hf-server.py
@@ -0,0 +1,344 @@
+from transformers import AutoModelForCausalLM, AutoTokenizer, pipeline, TextStreamer
+from huggingface_hub import login
+import torch
+
+import subprocess
+import traceback
+import argparse
+import logging
+import json
+import uuid
+import os
+
+from logging.handlers import RotatingFileHandler
+from flask import Flask, request, jsonify
+
+
+app = Flask(__name__)
+
+
+#########################------------Setup & Handle Logging-------------###############################
+try:
+    # 1 - Create a logger
+    logger = logging.getLogger('my_logger')
+    logger.setLevel(logging.ERROR)
+
+    # 2 - Create a RotatingFileHandler
+    # maxBytes: max file size of log file after which a new file is created; set to 1024 * 1024 * 5 for 5MB: 1024x1024 is 1MB, then a multiplyer for the number of MB
+    # backupCount: number of backup files to keep specifying how many old log files to keep
+    handler = RotatingFileHandler('hf_server_log.log', maxBytes=1024*1024*5, backupCount=2)
+    handler.setLevel(logging.ERROR)
+
+    # 3 - Create a formatter and set it for the handler
+    formatter = logging.Formatter('%(asctime)s:%(levelname)s:%(message)s')
+    handler.setFormatter(formatter)
+
+    # 4 - Add the handler to the logger
+    logger.addHandler(handler)
+    # Logger ready! Usage: logger.error(f"This is an error message with error {e}")
+except Exception as e:
+    print(f"\n\nCould not establish logger, encountered error: {e}")
+
+
+def handle_api_error(message, exception=None):
+    error_message = f"{message} {str(exception) if exception else '; No exception info.'}".strip()
+    traceback_details = traceback.format_exc()
+    full_message = f"\n\n{error_message}\n\nTraceback: {traceback_details}\n\n"
+
+    if logger:
+        logger.error(full_message)
+        print(error_message)
+    else:
+        print(full_message)
+    return jsonify(success=False, error=error_message), 500 #internal server error
+
+
+def handle_local_error(message, exception=None):
+    error_message = f"{message} {str(exception) if exception else '; No exception info.'}".strip()
+    traceback_details = traceback.format_exc()
+    full_message = f"\n\n{error_message}\n\nTraceback: {traceback_details}\n\n"
+
+    if logger:
+        logger.error(full_message)
+        print(error_message)
+    else:
+        print(full_message)
+    raise Exception(exception)
+
+
+def handle_error_no_return(message, exception=None):
+    error_message = f"{message} {str(exception) if exception else '; No exception info.'}".strip()
+    traceback_details = traceback.format_exc()
+    full_message = f"\n\n{error_message}\n\nTraceback: {traceback_details}\n\n"
+
+    if logger:
+        logger.error(full_message)
+        print(error_message)
+    else:
+        print(full_message)
+
+############################----------------------------------------------###############################
+
+
+
+############################------------configuration manager-------------###############################
+
+if not os.path.exists('hf_config.json'):
+    try:
+        with open('hf_config.json', 'w') as file:
+            json.dump({}, file)
+    except Exception as e:
+        handle_error_no_return("Could not init config.json. Multiple app restarts may be required to get the app to init correctly. Printing error and proceeding: ", e)
+
+
+# Method to write to hf_config.json | input- dict of key:values to be written to hf_config.json
+def write_config(config_updates, filename='hf_config.json'):
+
+    # Open hf_config file to read-in all current params:
+    try:
+        with open(filename, 'r') as file:
+            hf_config = json.load(file)
+    except Exception as e:
+        hf_config = {}     #init emply hf_config dict
+        handle_error_no_return("Could not read hf_config.json when attempting to write, encountered error: ", e)
+
+    #restart logic in write_config() might be unnecessary, circle back later
+    restart_required = False
+    triggers_for_hf_restart = ['torch_device_map', 'torch_dtype', 'model_id', 'trust_remote_code', 'attn_implementation', 'pipeline_task', 'max_new_tokens', 'return_full_text', 'temperature', 'do_sample', 'top_k', 'top_p', 'min_p', 'n_keep']
+    for key in config_updates:
+        if key in triggers_for_hf_restart and config_updates[key] != hf_config.get(key):
+            try:
+                #server_status = status()    #TO-DO
+                server_status = {'is_online': True}
+                if server_status['is_online']:
+                    restart_required = True
+            except Exception as e:
+                handle_error_no_return("Could not query hf_server status() for write_config, encountered error: ", e)
+
+    hf_config.update(config_updates)
+
+    # Write updated hf_config.json:
+    try:
+        with open(filename, 'w') as file:
+            json.dump(hf_config, file, indent=4)
+    except Exception as e:
+        handle_local_error("Could not update hf_config.json, encountered error: ", e)
+     
+    return {'success': True, 'restart_required':restart_required}
+            
+
+# Method to read from hf_config.json | input- list of keys to be read from hf_config.json; output- dict of key:value pairs; MANAGE DEFAULTS HERE!
+def read_config(keys, default_value=None, filename='hf_config.json'):
+    
+    # Open hf_config file to read-in all current params:
+    try:
+        with open(filename, 'r') as file:
+            hf_config = json.load(file)
+    except Exception as e:
+        handle_error_no_return("Could not read hf_config.json, encountered error: ", e)
+        return {key: default_value for key in keys}     #because a read scenario wherein hf_config.json does not exist shouldn't occur!
+    
+    return_dict = {}
+    update_config_dict = {}
+
+    for key in keys:
+        if key in hf_config:
+            return_dict[key] = hf_config[key]
+        else:
+            default_value = {
+                'access_gated':False,
+                'access_token':"",
+                'model_id':"microsoft/Phi-3-mini-4k-instruct", 
+                'torch_device_map':"auto", 
+                'torch_dtype':"auto", 
+                'trust_remote_code':False, 
+                'use_flash_attention_2':False, 
+                'pipeline_task':"text-generation", 
+                'max_new_tokens':500, 
+                'return_full_text':False, 
+                'temperature':0.0,
+                'do_sample':False, 
+                'top_k':40, 
+                'top_p':0.95, 
+                'min_p':0.05, 
+                'n_keep':0,
+            }.get(key, 'undefined')
+
+            if default_value == 'undefined':
+                raise KeyError(f"Key \'{key}\' not found in hf_config.json and no default value has been defined either.\n")
+            
+            return_dict[key] = default_value
+            update_config_dict[key] = default_value
+    
+    if update_config_dict:
+        # Write defaults
+        try:
+            write_config(update_config_dict)
+        except Exception as e:
+            handle_error_no_return("Could not write defaults to hf_config.json. Encountered error: ", e)
+
+    return return_dict
+
+
+# Method for API route to read from hf_config.json
+# Deviates from typical RESTful principals to use a POST call to fetch values but practical & justifyable because we:
+# 1. Do not want to make the URL huge with a ever-growing list of query-params 2. Do not wish to expose values via query-params
+@app.route('/hf_config_reader_api', methods=['POST'])
+def hf_config_reader_api():
+    # keys = request.args.getlist('keys') # Assuming keys are passed as query parameters
+    
+    try:
+        keys = request.json.get('keys', []) # Could also do keys = request.json['keys'] but this way we can provide a default list should 'keys' be missing!
+    except Exception as e:
+        handle_api_error("Server-side error - could not read keys for hf_config_reader_api request. Encountered error:", e)
+
+    try:
+        values = read_config(keys)  # send list of keys, get dict of key:values
+    except Exception as e:
+        handle_api_error("Server-side error - could not read keys from hf_config.json. Encountered error: ", e)
+    
+    return jsonify(success=True, values=values)
+
+
+# Method for API route to write to hf_config.json
+@app.route('/hf_config_writer_api', methods=['POST'])
+def hf_config_writer_api():
+
+    try:
+        config_updates = request.json['config_updates']
+        print(f"config_updates for hf_config_writer_api: {config_updates}")
+    except Exception as e:
+        handle_api_error("Server-side error - could not read values for hf_config_writer_api request. Encountered error: ", e)
+    
+    try:
+        write_return = write_config(config_updates)
+    except Exception as e:
+        handle_api_error("Server-side error - could not write keys to hf_config.json. Encountered error: ", e)
+    
+    return jsonify({"success": write_return['success'], "restart_required": write_return['restart_required']})
+
+
+############################----------------------------------------------###############################
+
+
+def hf_login_for_gated_models():
+    access_token = ""
+    try:
+        read_return = read_config(['access_token'])
+        access_token = str(read_return['access_token'])
+    except Exception as e:
+        handle_api_error("403 - No access token found, please submit an access token via the /hf_login endpoint")
+
+    try:
+        login(token=access_token)
+    except Exception as e:
+        handle_api_error("Unable to login to the HuggingFace-Hub, please ensure the correct access token has been provided. Encountered error: ", e)
+
+
+def parse_arguments():
+
+    try:
+        parser = argparse.ArgumentParser(description="Server for HuggingFace Transformers models")
+    except Exception as e:
+        handle_api_error("Could not create parser to parse_arguments(), proceeding with defaults. Encountered error: ", e)
+
+    # Even if a parser object could not be created, a read_request will write & return defaults 
+    try:
+        read_return = read_config(['access_gated', 'access_token', 'model_id', 'torch_device_map', 'torch_dtype', 'trust_remote_code', 'use_flash_attention_2', 'pipeline_task', 'max_new_tokens', 'return_full_text', 'temperature', 'do_sample', 'top_k', 'top_p', 'min_p', 'n_keep'])
+        access_gated = bool(read_return['access_gated'])
+        access_token = str(read_return['access_token'])
+        model_id = str(read_return['model_id'])
+        torch_device_map = str(read_return['torch_device_map'])
+        torch_dtype = str(read_return['torch_dtype'])
+        trust_remote_code = bool(read_return['trust_remote_code'])
+        use_flash_attention_2 = bool(read_return['use_flash_attention_2'])
+        pipeline_task = str(read_return['pipeline_task'])
+        max_new_tokens = int(read_return['max_new_tokens'])
+        return_full_text = bool(read_return['return_full_text'])
+        temperature = float(read_return['temperature'])
+        do_sample = bool(read_return['do_sample'])
+        top_k = int(read_return['top_k'])
+        top_p = float(read_return['top_p'])
+        min_p = float(read_return['min_p'])
+        n_keep = int(read_return['n_keep'])
+    except Exception as e:
+        handle_local_error("Could not read values from hf_config.json when trying to parse_arguments(), encountered error: ", e)
+
+    if parser:
+
+        parser.add_argument("--reset_to_defaults", action="store_true", default=False, help="Use default settings")
+        parser.add_argument("--access_gated", action="store_true", default=access_gated, help="Specify True if you will be accessing gated models you've been approved to access")
+        parser.add_argument("--access_token", type=str, default=access_token, help="Access Token obtained from HF-Settings -> Access Tokens")
+        parser.add_argument("--model_id", type=str, default=model_id, help="model_id for for LLM in HF-Transformers format obtained from the model card. Remembers previously set value and falls-back to Phi3-mini-4k-instruct as the default.")
+        parser.add_argument("--torch_device_map", type=str, default=torch_device_map, help="Specify inference device, example: cuda. Remembers previously set value and falls-back to auto as the default.")
+        parser.add_argument("--torch_dtype", type=str, default=torch_dtype, help="Specify model tensor type, example: bfloat16. Remembers previously set value and falls-back to auto as the default.")
+        parser.add_argument("-trust_remote_code", action="store_true", default=trust_remote_code, help="Allows the model to execute custom code that's part of the model's HF-repository. Remembers previously set value and falls-back to False by default as a security measure to prevent potentially malicious code from running automatically.")
+        parser.add_argument("--use_flash_attention_2", action="store_true", default=use_flash_attention_2, help="Set to True to attempt using Flash Attention 2. Remembers previously set value and falls-back to False as a default. Failed attempt to use FA2 will proceed to load the model without FA2.")
+        parser.add_argument("--pipeline_task", type=str, default=pipeline_task, help="Defaults to text-generation. For more details, open a Python shell, `import transformers`, and Run `help(transfomers.pipeline)`.")
+        parser.add_argument("--max_new_tokens", type=int, default=max_new_tokens, help="Set a hard limit on the maximum number of tokens an LLM can generate when responding. Remembers previously set value and falls-back to 500 as a default.")
+        parser.add_argument("--return_full_text", action="store_true", default=return_full_text, help="When set to True, the LLM response contains the entire messages list with the latest response appended at the end.")
+        parser.add_argument("--temperature", type=float, default=temperature, help="Set LLM temperature on a scale of 0.0 to 2.0. Remembers previously set value and falls-back to 0.0 as a default.")
+        parser.add_argument("--do_sample", action="store_true", default=do_sample, help="Perform sampling when selecting response tokens. Remembers previously set value and falls-back to Flase as a default. Must be set to True when temperature is above 0.0. For greedy decoding, leave this as False and set temp to 0.0")
+        parser.add_argument("--top_k", type=int, default=top_k, help="Limit the next token selection to the K most probable tokens. Remembers previously set value and falls-back to 40 as a default.")
+        parser.add_argument("--top_p", type=float, default=top_p, help="Limit the next token selection to a subset of tokens with a cumulative probability above a threshold P. Remembers previously set value and falls-back to 0.95 as a default.")
+        parser.add_argument("--min_p", type=float, default=min_p, help="The minimum probability for a token to be considered, relative to the probability of the most likely token. Remembers previously set value and falls-back to 0.05 as a default.")
+        parser.add_argument("--n_keep", type=int, default=n_keep, help="Specify the number of tokens from the prompt to retain when the context size is exceeded and tokens need to be discarded. Remembers previously set value and falls-back to 0 as a default, meaning no tokens are kept. Use -1 to retain all tokens from the prompt.")
+
+        args = parser.parse_args()
+        print(f"\n\nparser.parse_args():\n\n{args}\n\n")
+
+        if args.reset_to_defaults:
+
+            try:
+                # Empty hf_config.json
+                with open('hf_config.json', 'w') as file:
+                    json.dump({}, file, indent=4)
+                
+                # Set defaults
+                read_config(['access_gated', 'access_token', 'model_id', 'torch_device_map', 'torch_dtype', 'trust_remote_code', 'use_flash_attention_2', 'pipeline_task', 'max_new_tokens', 'return_full_text', 'temperature', 'do_sample', 'top_k', 'top_p', 'min_p', 'n_keep'])
+
+            except Exception as e:
+                handle_local_error("Could not reset hf_config.json, encountered error: ", e)
+        else:
+            try:
+                write_config({
+                    'access_gated':args.access_gated,
+                    'access_token':args.access_token,
+                    'model_id':args.model_id, 
+                    'torch_device_map':args.torch_device_map, 
+                    'torch_dtype':args.torch_dtype, 
+                    'trust_remote_code':args.trust_remote_code, 
+                    'use_flash_attention_2':args.use_flash_attention_2, 
+                    'pipeline_task':args.pipeline_task, 
+                    'max_new_tokens':args.max_new_tokens, 
+                    'return_full_text':args.return_full_text, 
+                    'temperature':args.temperature,
+                    'do_sample':args.do_sample, 
+                    'top_k':args.top_k, 
+                    'top_p':args.top_p, 
+                    'min_p':args.min_p, 
+                    'n_keep':args.n_keep
+                })
+            except Exception as e:
+                handle_local_error("Could not write launch arguments to hf_config.json, encountered error: ", e)
+
+            if args.access_gated:
+                try:
+                    hf_login_for_gated_models()
+                except Exception as e:
+                    handle_local_error("Login to HF-Hub unsuccessful, encountered error: ", e)
+
+    #return parser.parse_args()
+    return True
+
+
+def main():
+    #TODO
+
+    parse_arguments()
+
+    app.run(host='0.0.0.0', port=9069)
+
+
+if __name__ == '__main__':
+    main()
